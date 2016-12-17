2e1b96a00d92bd98d63c42aecbcf55eb7ac8a93d
caboe2012 <chad.aboe@gmail.com>
2016-12-17 23:58:36 +0000

    Complete Extension Exercises

--- bartender.py
+++ bartender.py
@@ -16,7 +16,6 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
-
 ingredient_count = {}
 
 segues = ["What else..", "Okay" ,"Let's see...", "Hmmm...", "Well then...", 
@@ -24,8 +23,12 @@ segues = ["What else..", "Okay" ,"Let's see...", "Hmmm...", "Well then...",
 
 drink_adjs = ["Rabid", "Sea-Sick", "Shredded", "Tired", "Flaccid", "Two-Faced", "Broken", "Restless", "Tireless", "Unbeatable"]
 
-drink_nouns = ["Leviathon", "Krackin'", "White-Whale", "Moby Dick", "Sea-Legs", "Land-Lubbarrr", "Will-o-the-Wisp", "Siren",
+drink_nouns = ["Leviathon", "Krackin'", "White-Whale", "Moby Dick", "Sea-Leg", "Land-Lubbarrr", "Will-o-the-Wisp", "Siren",
               "Treasure Chest", "Treasure Map"]
+
+adverbs = ["rarely", "try not to", "never", "sometimes", "don't often", "barely ever"]
+
+similies = ["Elephant Seal", "Sperm Whale", "Tiger Shark", "Bottle Nose Dolphin", "Sea Urchin"]
 
 customers = {}
 
@@ -33,30 +36,27 @@ patrons = []
 
 def ask_name():
     repeat = False
-    print "Ahoy! My name's Max.  What's yours?"
-    print
+    print "Ahoy! Who goes there?"
     name = raw_input()
     another = True
     if name in patrons:
         repeat = True
         print "Of course,", name, "I see you like my drinks ya ol sea-dog!"
-        print "I'm like an Elephant Seal, I never forget"
-        print "As I recall ya like yer drinks " + ", ".join([pref for pref in customers[name]])
+        print "I'm like a", random.choice(similies) +"," + " I", random.choice(adverbs), "forget!"
+        print "As I recall, ya like yer drinks " + ", ".join([pref for pref in customers[name]])
         print "Would you like me to mix another elixir for yee?"
         una_mas = raw_input()
         print
         if una_mas.lower() in ["y", "yes"]:
             print "Una mas it is!"
-            for each in customers[name]:
-                ingredient_count[each] = ingredient_count.get(each, 0) + 1
-            print ingredient_count
         else:
             another = False
             print "Alrighty then, thanks for stopping by, have a nice night!"
             print
     else:            
         patrons.append(name)
-        print "Avast, welcome to my humble priate-bar", name, "pleasure to make yer acquaintance."
+        print
+        print "Avast! Welcome to my humble priate-bar, " +  name +"," +" pleasure to make yer acquaintance."
     return [repeat, name, another]
 
 def ask_questions(name):
@@ -111,23 +111,21 @@ if __name__ == "__main__":
         repeat_customer = ask_name()
         if not repeat_customer[0]:    
             answers = ask_questions(repeat_customer[1])
-            print make_drink(answers)
         if repeat_customer[2]:
+            make_drink(answers)
             name_drink()
         else:
             # remove the person from the patron list for that night
             patrons.remove(repeat_customer[1])
         print
         n = len(patrons)
-        print "Patrons", patrons
-        print "Number of patrons left", n
         check += 1
         if check > 10:
             break
     print 
-    if check > 5:
+    if check > 10:
         print "Wow, look at the time!"
     else:
-        print "Looks like I'm all outta customers for tonight"
+        print "Looks like I'm all outta customers for tonight."
     print "Time to close up shop and head home."
         
