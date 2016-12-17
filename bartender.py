ab32404c2a929d7ac32687802022092fbc60f4f3
caboe2012 <chad.aboe@gmail.com>
2016-12-17 22:50:44 +0000

    Extension Exercises

--- bartender.py
+++ bartender.py
@@ -16,15 +16,50 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 
+
+ingredient_count = {}
+
 segues = ["What else..", "Okay" ,"Let's see...", "Hmmm...", "Well then...", 
           "Let's get some more ideas..."]
 
-drink_adjs = ["Rabid", "Sea-Sick", "Shredded", "Tired", "Two-Faced", "Broken", "Restless", "Tireless", "Unbeatable"]
+drink_adjs = ["Rabid", "Sea-Sick", "Shredded", "Tired", "Flaccid", "Two-Faced", "Broken", "Restless", "Tireless", "Unbeatable"]
 
 drink_nouns = ["Leviathon", "Krackin'", "White-Whale", "Moby Dick", "Sea-Legs", "Land-Lubbarrr", "Will-o-the-Wisp", "Siren",
               "Treasure Chest", "Treasure Map"]
 
-def ask_questions():
+customers = {}
+
+patrons = []
+
+def ask_name():
+    repeat = False
+    print "Ahoy! My name's Max.  What's yours?"
+    print
+    name = raw_input()
+    another = True
+    if name in patrons:
+        repeat = True
+        print "Of course,", name, "I see you like my drinks ya ol sea-dog!"
+        print "I'm like an Elephant Seal, I never forget"
+        print "As I recall ya like yer drinks " + ", ".join([pref for pref in customers[name]])
+        print "Would you like me to mix another elixir for yee?"
+        una_mas = raw_input()
+        print
+        if una_mas.lower() in ["y", "yes"]:
+            print "Una mas it is!"
+            for each in customers[name]:
+                ingredient_count[each] = ingredient_count.get(each, 0) + 1
+            print ingredient_count
+        else:
+            another = False
+            print "Alrighty then, thanks for stopping by, have a nice night!"
+            print
+    else:            
+        patrons.append(name)
+        print "Avast, welcome to my humble priate-bar", name, "pleasure to make yer acquaintance."
+    return [repeat, name, another]
+
+def ask_questions(name):
     ans_dict = {}
     counter = 0
     for k,v in questions.items():
@@ -42,6 +77,7 @@ def ask_questions():
         if counter <=4:
             print random.choice(segues)
             print
+    customers[name] = [k for k,v in ans_dict.items() if v == True]
     print "One sec and I'll have just the right drink ready for ya..."
     print
     return ans_dict
@@ -51,27 +87,47 @@ def make_drink(Ans):
     for k,v in Ans.items():
         if v:
             temp = random.choice(ingredients[k])
+            ingredient_count[temp] = ingredient_count.get(temp, 0) + 1
             drink.append(temp)
     print "There ya go matey!"
-    print
     return drink
 
 def name_drink():
     print "House special...I call it a", random.choice(drink_adjs), random.choice(drink_nouns)
     print "Enjoy yer libation!"
+    need_more = [k for k,v in ingredient_count.items() if v > 3]
+    if len(need_more) > 0:
+        print "Time to restock the " + ", ".join([more for more in need_more])
+        print "Be right back...and stay thirsty friends"
+    print
+    
 
 if __name__ == "__main__":
     thirsty = True
-    while thirsty:
-        answers = ask_questions()
-        print make_drink(answers)
-        name_drink()
-        print
-        print "I see you liked the drink ya ol sea-dog! Can I get yah anothun?"
-        result = raw_input()
-        if result.lower() in ["y", "yes"]:
-            print "Una mas it is."
-            print
+    repeat_customer = False
+    n = 1
+    check = 0
+    while n > 0:
+        repeat_customer = ask_name()
+        if not repeat_customer[0]:    
+            answers = ask_questions(repeat_customer[1])
+            print make_drink(answers)
+        if repeat_customer[2]:
+            name_drink()
         else:
-            thirsty = False
+            # remove the person from the patron list for that night
+            patrons.remove(repeat_customer[1])
+        print
+        n = len(patrons)
+        print "Patrons", patrons
+        print "Number of patrons left", n
         check += 1
+        if check > 10:
+            break
+    print 
+    if check > 5:
+        print "Wow, look at the time!"
+    else:
+        print "Looks like I'm all outta customers for tonight"
+    print "Time to close up shop and head home."
+        
